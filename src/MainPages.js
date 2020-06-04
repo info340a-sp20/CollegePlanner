c205367b91aaf61b3a746dcb52a4a8756f43d9da
sunnycucu <s1u2n1n1y@gmail.com>
2020-06-04 01:59:14 -0700

    save data

--- src/MainPages.js
+++ src/MainPages.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import { NavigationBar } from './NavigationBar.js';
 import { ChangeViewButton } from './ChangeViewButton.js';
+import { NavLink, BrowserRouter } from 'react-router-dom';
 
-import {Footer} from './Footer.js';
-import {AllSchedule} from './AllSchedule.js';
+import { Footer } from './Footer.js';
+import { AllSchedule } from './AllSchedule.js';
 
-import{Calander} from './Calander.js';
+import { Calander } from './Calander.js';
 export class CalanderView extends Component {
     render() {
         return (
             <span>
-                <NavigationBar handleSignOut={this.props.handleSignOut}/>
+                <NavigationBar handleSignOut={this.props.handleSignOut} />
                 <h3>Hello,  {this.props.userName}</h3>
                 <h4>Plan your work and work your plan.</h4>
-                <ChangeViewButton view='Memo View' path='MemoView' />
+                <span className="button-container">
 
-                <Calander state={this.props.state} searchCallback={this.props.searchCallback}/>
+                    <div className="searchBox" role="search">
+                    </div>
+                    <NavLink className="btn btn-secondary btn-sm" to={'/MemoView'} role="button" >Memo View</NavLink>
+                </span>
+                <Calander state={this.props.state} searchCallback={this.props.searchCallback} />
                 <Footer />
             </span>
         );
@@ -26,12 +31,12 @@ export class MemoView extends Component {
         console.log(this.props.state);
         return (
             <span>
-                <NavigationBar handleSignOut={this.props.handleSignOut}/>
+                <NavigationBar handleSignOut={this.props.handleSignOut} />
                 <h3>Hello, {this.props.userName} </h3>
                 <h4>Plan your work and work your plan.</h4>
 
-                <ChangeViewButton view='Calander View' path='CalanderView' state={this.props.state} searchCallback={this.props.searchCallback}/>
-                <AllSchedule state={this.props.state} searchCallback={this.props.searchCallback}  />
+                <ChangeViewButton view='Calander View' path='CalanderView' state={this.props.state} searchCallback={this.props.searchCallback} />
+                <AllSchedule state={this.props.state} searchCallback={this.props.searchCallback} />
 
                 <Footer />
             </span>
